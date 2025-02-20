ca2ddc6642c9cb5911eb0a44a091b8df4da504bf
michellevillaca <michellevandry@gmail.com>
2025-02-20 20:00:06 -0300

    Início de construção do código JS

--- aula12ex/ex016/script.js
+++ aula12ex/ex016/script.js
@@ -1,1 +1,11 @@
-alert('Olá')
+function verificar() {
+    var data = new Date() //data atual
+    var ano = data.getFullYear() //ano atual (full year é porque pega os 4 dígitos)
+    var fAno = document.getElementById('txtano') //caixa de texto do ano de nascimento
+    var res = document.querySelector('div#res')
+    if (fAno.value.lenght == 0 || fAno.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente.')
+    } else {
+        window.alert('Tudo OK.')
+    }
+}
