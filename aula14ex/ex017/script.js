b4f09313072deb8b0e788a83ed4974abd87fbbe9
michellevillaca <michellevandry@gmail.com>
2025-02-25 19:01:14 -0300

    Resolução completa do exercício de contagem!
    *correção de erros

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,15 +1,37 @@
 function contar() {
-    var i = document.getElementById('txtstart')
-    var f = document.getElementById('txtend')
-    var p = document.getElementById('txtstep')
-    var c = i
-    var res = document.getElementById('res')
-    if(i.value.length == 0 || f.value.length == 0 || p.value.length == 0) {
+    let inicio = document.getElementById('txtstart')
+    let fim = document.getElementById('txtend')
+    let passo = document.getElementById('txtstep')
+    let res = document.getElementById('res')
+
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados.')
         res.innerHTML = 'Impossível contar!'
     } else {
-        while (c <= f) {
-            res.innerHTML = `${c}`
-            c += p
+        res.innerHTML = 'Contando: '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        let c = i
+
+        if (i < f) {
+            // Contagem crescente:
+             while (c <= f) {
+                res.innerHTML += `${c} \u{1F449} `
+                c += p
+            }
+                /*
+                for(let c=i; c<=f; c+=p) {
+                    res.innerHTML += `${c} `
+                } */
+        } else {
+            // Contagem regressiva:
+            while (c >= f) {
+                res.innerHTML += `${c} \u{1F449} `
+                c -= p
+            }
         }
+        res.innerHTML += `\u{1F3C1}`
+
     }
 }
