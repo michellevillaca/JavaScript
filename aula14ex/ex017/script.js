571908e1b36a9d02cd8e0a746470f83b1934e935
michellevillaca <michellevandry@gmail.com>
2025-02-25 18:37:50 -0300

    Tentativa de resolução de exercício
    Cálculos corretos, porém não sai o resultado no res.innerHTML

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -2,8 +2,14 @@ function contar() {
     var i = document.getElementById('txtstart')
     var f = document.getElementById('txtend')
     var p = document.getElementById('txtstep')
+    var c = i
     var res = document.getElementById('res')
     if(i.value.length == 0 || f.value.length == 0 || p.value.length == 0) {
         res.innerHTML = 'Impossível contar!'
+    } else {
+        while (c <= f) {
+            res.innerHTML = `${c}`
+            c += p
+        }
     }
 }
---
+++ aula14ex/ex017/teste.js
@@ -0,0 +1,8 @@
+var i = 5
+var f = 50
+var p = 5
+var c = i
+while (c <= f) {
+    console.log(c)
+    c += p
+}
