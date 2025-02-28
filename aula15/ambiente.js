786f8b1ce5982251ccb4b6342d20887bfdfc8654
michellevillaca <michellevandry@gmail.com>
2025-02-27 21:32:39 -0300

    Aula 15 - testando variáveis compostas

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5,9,2,4,3]
+num[5] = 6
+num.push(7)
+
+console.log(`Nosso vetor é o ${num}`)
+console.log(`A minha variável tem ${num.length} elementos.`)
+console.log(`Em ordem crescente, tenho os números ${num.sort()}`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
