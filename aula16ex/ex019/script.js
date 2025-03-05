b0dfc71529ca04c276e2c29008fcf30f44e2943c
michellevillaca <michellevandry@gmail.com>
2025-03-05 20:34:13 -0300

    Tentativa de fazer o exercício
    *aqui é o momento em que eu vejo o vídeo de resolução kkkk (não consegui encontrar o modo de colocar os números digitados dentro de um array)

--- aula16ex/ex019/script.js
+++ aula16ex/ex019/script.js
@@ -1,11 +1,19 @@
 let num = document.getElementById('txtnum')
 let tab = document.getElementById('seltab')
+let res = document.getElementById('res')
+let valores = []
 
 function adicionar() {
     let n = Number(num.value)
     if (n > 100 || n < 0 || num.value.length == 0) {
         window.alert('ERRO! Por favor, digite um número válido.')
     } else {
-        let c = 1
+        let item = document.createElement('option')
+        item.text = `Valor ${n} adicionado.`
+        tab.appendChild(item)
     }
+}
+
+function finalizar() {
+    res.innerHTML = `Saudações`
 }
