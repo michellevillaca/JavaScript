51bd3563992d88da854026744e1a99ba16f344d7
michellevillaca <michellevandry@gmail.com>
2025-03-07 20:06:52 -0300

    Finalizando o código

--- testes/01/script.js
+++ testes/01/script.js
@@ -5,6 +5,7 @@ let sexo = document.getElementsByName('fsexo')
 let peso = document.getElementById('fpeso')
 let lista = document.getElementById('flista') // Adicionando referência à lista
 let pessoas = []
+let res = document.getElementById('res')
 
 function cadastrar() {
     let sexoSelecionado = ''
@@ -35,4 +36,36 @@ function cadastrar() {
     nascimento.value = ''
     peso.value = ''
     nome.focus() // Mantém o foco no primeiro campo para novo cadastro
+}
+
+function finalizar() {
+    if (pessoas.length == 0) {
+        window.alert('Adicione valores antes de finalizar.')
+    } else {
+        let tot = pessoas.length
+        let maiorpeso = pessoas[0].peso
+        let menorpeso = pessoas[0].peso
+        let maisvelho = pessoas[0].idade
+        let maisnovo = pessoas[0].idade
+
+        for (let pos in pessoas) {
+            if (pessoas[pos].peso > maiorpeso) {
+                maiorpeso = pessoas[pos].peso
+            }
+            if (pessoas[pos].peso < menorpeso) {
+                menorpeso = pessoas[pos].peso
+            }
+            if (pessoas[pos].idade > maisvelho) {
+                maisvelho = pessoas[pos].idade
+            }
+            if (pessoas[pos].idade < maisnovo) {
+                maisnovo = pessoas[pos].idade
+            }
+        }
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} pessoas cadastradas.</p>`
+        res.innerHTML += `<p>A pessoa de maior massa corporal pesa ${maiorpeso}Kg, e a de menor massa corporal pesa ${menorpeso}Kg.</p>`
+        res.innerHTML += `<p>A pessoa mais velha tem ${maisvelho} anos, enquanto a mais nova tem ${maisnovo} anos.</p>`
+    }
 }
