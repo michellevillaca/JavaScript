bf9332a26aa6596aa4483ebbf5f2f9a1584db4ee
michellevillaca <michellevandry@gmail.com>
2025-03-07 19:01:55 -0300

    Correção de código com ajuda do chatgpt

--- testes/01/script.js
+++ testes/01/script.js
@@ -3,11 +3,36 @@ let idade = document.getElementById('fidade')
 let nascimento = document.getElementById('fnasc')
 let sexo = document.getElementsByName('fsexo')
 let peso = document.getElementById('fpeso')
+let lista = document.getElementById('flista') // Adicionando referência à lista
 let pessoas = []
 
 function cadastrar() {
-    pessoas.push(nome, Number(idade.value), nascimento, sexo, Number(peso.value))
+    let sexoSelecionado = ''
+    for (let s of sexo) {
+        if (s.checked) {
+            sexoSelecionado = s.value
+            break
+        }
+    }
+
+    let pessoa = {
+        nome: nome.value,
+        idade: Number(idade.value),
+        nascimento: nascimento.value,
+        sexo: sexoSelecionado,
+        peso: Number(peso.value)
+    }
+
+    pessoas.push(pessoa)
+
     let item = document.createElement('option')
-    item.text = `${nome} cadastrado com sucesso.`
+    item.text = `${pessoa.nome} cadastrado com sucesso.`
     lista.appendChild(item)
+
+    // Limpar campos após o cadastro
+    nome.value = ''
+    idade.value = ''
+    nascimento.value = ''
+    peso.value = ''
+    nome.focus() // Mantém o foco no primeiro campo para novo cadastro
 }
