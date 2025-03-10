67da24e16114d5789b6c53b02939ac10a32057a9
michellevillaca <michellevandry@gmail.com>
2025-03-09 22:04:46 -0300

    Aprimoramento no exercício teste

--- testes/01/script.js
+++ testes/01/script.js
@@ -43,29 +43,40 @@ function finalizar() {
         window.alert('Adicione valores antes de finalizar.')
     } else {
         let tot = pessoas.length
+
         let maiorpeso = pessoas[0].peso
         let menorpeso = pessoas[0].peso
         let maisvelho = pessoas[0].idade
         let maisnovo = pessoas[0].idade
 
+        let nomeMaiorPeso = pessoas[0].nome
+        let nomeMenorPeso = pessoas[0].nome
+        let nomeMaisNovo = pessoas[0].nome
+        let nomeMaisVelho = pessoas[0].nome
+
+
         for (let pos in pessoas) {
             if (pessoas[pos].peso > maiorpeso) {
                 maiorpeso = pessoas[pos].peso
+                nomeMaiorPeso = pessoas[pos].nome
             }
             if (pessoas[pos].peso < menorpeso) {
                 menorpeso = pessoas[pos].peso
+                nomeMenorPeso = pessoas[pos].nome
             }
             if (pessoas[pos].idade > maisvelho) {
                 maisvelho = pessoas[pos].idade
+                nomeMaisVelho = pessoas[pos].nome
             }
             if (pessoas[pos].idade < maisnovo) {
                 maisnovo = pessoas[pos].idade
+                nomeMaisNovo = pessoas[pos].nome
             }
         }
 
         res.innerHTML = ''
-        res.innerHTML += `<p>Ao todo temos ${tot} pessoas cadastradas.</p>`
-        res.innerHTML += `<p>A pessoa de maior massa corporal pesa ${maiorpeso}Kg, e a de menor massa corporal pesa ${menorpeso}Kg.</p>`
-        res.innerHTML += `<p>A pessoa mais velha tem ${maisvelho} anos, enquanto a mais nova tem ${maisnovo} anos.</p>`
+        res.innerHTML += `<p>Ao todo temos <strong>${tot}</strong> pessoas cadastradas.</p>`
+        res.innerHTML += `<p>A pessoa de maior massa corporal pesa <strong>${maiorpeso}Kg</strong>, que é o/a <strong>${nomeMaiorPeso}</strong>, enquanto a de menor massa corporal pesa <strong>${menorpeso}Kg</strong>, que é o/a <strong>${nomeMenorPeso}</strong>.</p>`
+        res.innerHTML += `<p>A pessoa mais velha tem <strong>${maisvelho} anos</strong>, que é o/a <strong>${nomeMaisVelho}</strong>, enquanto a mais nova tem <strong>${maisnovo} anos</strong>, que é o/a <strong>${nomeMaisNovo}</strong>.</p>`
     }
 }
